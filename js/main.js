50c84271cc8663a9dd5429694d721112af8c5617
DIVY <divyadeepsawhney@hotmail.com>
2015-07-09 01:00:16 +0530

    Added Menu Bar

--- js/main.js
+++ js/main.js
@@ -30,3 +30,29 @@ document.getElementById('save').addEventListener('change', function (e) {
 	file.save(this.value, document);
 });
 }
+
+//Menu Bar
+var menu = new gui.Menu({ type: 'menubar' });
+
+menu.append(new gui.MenuItem({
+	label: 'File',
+	submenu: new gui.Menu()
+}));
+
+menu.items[0].submenu.append(new gui.MenuItem({
+	label: 'New',
+	click: function () {
+		gui.Window.open('index.html');
+	}
+}));
+menu.items[0].submenu.append(new gui.MenuItem({
+	type: 'separator'
+}));
+menu.items[0].submenu.append(new gui.MenuItem({
+	label: 'Close',
+	click: function () {
+		gui.Window.get().close();
+	}
+}));
+
+gui.Window.get().menu = menu;
