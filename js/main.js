426220a1182081cf098d62e9053f59e234ed74ab
DIVY <divyadeepsawhney@hotmail.com>
2015-07-09 00:53:07 +0530

    Working Dummy

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
 var file = require('file.js');
 
-console.log(file.open, file.save);
+function clickInput(id) {
+	var event = document.createEvent('MouseEvents');
+	event.initMouseEvent('click');
+	document.getElementById(id).dispatchEvent(event);
+}
+
+document.addEventListener('keyup', function (e) {
+	if (e.keyCode == 'O'.charCodeAt(0) && e.ctrlKey) {
+		clickInput('open');
+	} else if (e.keyCode == 'S'.charCodeAt(0) && e.ctrlKey) {
+		clickInput('save');
+	}
+});
+
+//if(document.getElementById('open')){
+document.getElementById('open').addEventListener('change', function (e) {
+    
+	file.open(this.value, document);
+    //console.log("I run");
+});
+//}
+
+if(document.getElementById('save')){
+document.getElementById('save').addEventListener('change', function (e) {
+	file.save(this.value, document);
+});
+}
